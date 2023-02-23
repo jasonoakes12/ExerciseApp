3146e3d1d51398eb871ea139b2946d44f48cf9e7
jasonoakes12 <112090261+jasonoakes12@users.noreply.github.com>
2023-02-23 18:40:48 -0500

    Lab6

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import RepetitionExercise from "./components/RepetitionExercise";
+import DurationExercise from "./components/DurationExercise";
+
+const exercises = [
+  {
+    name: "Pushups",
+    type: "repetition",
+    component: <RepetitionExercise name="Pushups" />
+  },
+  {
+    name: "Plank",
+    type: "duration",
+    component: <DurationExercise name="Plank" />
+  },
+  {
+    name: "Squats",
+    type: "repetition",
+    component: <RepetitionExercise name="Squats" />
+  },
+  {
+    name: "Running",
+    type: "duration",
+    component: <DurationExercise name="Running" />
+  },
+];
+
+function App() {
+  const [currentExercise, setCurrentExercise] = useState(null);
+
+  const handleExerciseClick = (exercise) => {
+    setCurrentExercise(exercise.component);
+  };
+
+  const renderExerciseMenu = () => {
+    return exercises.map((exercise, index) => {
+      return (
+        <button key={index} onClick={() => handleExerciseClick(exercise)}>
+          {exercise.name}
+        </button>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Lab 6 Exercise App</h1>
+      <div>
+        {currentExercise ? (
+          <div>
+
+            {currentExercise}
+          </div>
+        ) : (
+          <div>
+            <h2>Choose an exercise!</h2>
+            {renderExerciseMenu()}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
